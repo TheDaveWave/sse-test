9b57aaaaa7ec1b031e28b130355676916466e1aa
David Lindberg <david.wesley.lindberg@gmail.com>
2024-11-17 18:10:14 -0600

    got something working

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-const eventSource = new EventSource("localhost:3000/sse");
+document.addEventListener("DOMContentLoaded", () => onReady());
+const eventSource = new EventSource("http://localhost:3000/sse");
+
+async function onReady() {
+    window.addEventListener("beforeunload", () => {eventSource.close();});
+    try {
+        const result = await fetch("http://localhost:3000/events", {method: "POST", headers: {'Content-Type': 'application/json'}, body: JSON.stringify({"number": 10})});
+        console.log("RESULT:", result);
+    } catch (err) {
+        console.error("Error in onReady:", err);
+    }
+}
+
+eventSource.addEventListener("message", (event) => {
+    const el = document.getElementById("numbers");
+    const elText = el.innerText;
+    el.innerText = elText + " " + event.data;
+    console.log("Getting data...", event.data);
+});
+
+eventSource.onerror = (err) => {
+    console.error("Error in EventSource:", err);
+}
 
--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 const PORT = process.env.PORT || 3000;
 
 const clients = [];
+
+// Live Server Extension Port:
+// app.use(cors({origin: "http://localhost:5500", credentials: true}));
+app.use(cors({origin: "http://127.0.0.1:5500", credentials: true}));
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.get("/sse", (req, res) => {
     res.writeHead(200, {
@@ -14,12 +22,21 @@ app.get("/sse", (req, res) => {
 
     clients.push(res);
 
-    res.write('data: Initialized Connection:\n');
+    res.write('data: Initialized Connection:\n\n');
+
+    // clients.forEach(async (client) => {
+    //     for(let numEvents = 1; numEvents <= 10; numEvents++) {
+    //         client.write(`data: ${numEvents}`);
+    //         await delay(1000, numEvents);
+    //     }
+    // });
 
     req.on("close", () => {
         if(clients.indexOf(res) < 0) { return; }
         clients.splice(clients.indexOf(res), 1);
     });
+
+    // res.json({"message": `Events Sent.`});
 });
 
 function delay(ms, value) {
@@ -31,12 +48,14 @@ function delay(ms, value) {
 }
 
 app.post("/events", async (req, res) => {
-    const { number } = req.body;
-    if(!number) { res.sendStatus(400); }
+    const number = req.body?.number;
+    // if(!number) { res.sendStatus(400); }
     clients.forEach(async (client) => {
+        console.log("CLIENT", number);
         for(let numEvents = 1; numEvents <= number; numEvents++) {
-            client.write(`data: ${numEvents}`);
-            await delay(2000, numEvents);
+            client.write(`data: ${numEvents}\n\n`);
+            console.log("Writing to client:", `data: ${numEvents}`);
+            await delay(1000, numEvents);
         }
     });
     res.json({"message": `${number} Events Sent.`});
